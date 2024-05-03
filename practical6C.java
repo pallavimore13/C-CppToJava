1c236c008808e3103cc7d5b5a58bb2289157e768
pallavimore13 <2022bit151@sggs.ac.in>
2024-05-03 14:29:29 -0400

    Program on switch case

---
+++ practical6C.java
@@ -0,0 +1,25 @@
+class Main
+{
+	public static void main(String[] args)
+	{
+		int value = 3;
+		
+		switch(value)
+		{
+			case 1:
+				System.out.println("Value is 1");
+				break;
+			case 2:
+				System.out.println("Value is 2");
+				break;
+			case 3:
+				System.out.println("Value is 3");
+				break;
+			case 4:
+				System.out.println("Value is 4");
+				break;
+			default:
+				System.out.println("Value is other than 1,2,3,4");
+		}
+	}
+}
