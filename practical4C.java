c08f328d8909ead0dcd7761c1c9bfe27e7b3ab5d
pallavimore13 <2022bit151@sggs.ac.in>
2024-05-03 14:19:41 -0400

    Program for if-else statement

---
+++ practical4C.java
@@ -0,0 +1,22 @@
+class Main
+{
+	public static void main(String[] args)
+	{
+		int m = 40;
+		int n = 20;
+		
+		if(m > n){
+			System.out.println("m is greater than n");
+		}
+		else if(m < n)
+		{
+			System.out.println("m is less than n");
+		}
+
+		else
+		{
+			System.out.println("m is equal to n");
+		}
+		
+	}
+}
